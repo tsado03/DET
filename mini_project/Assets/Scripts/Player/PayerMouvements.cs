284844a582abf759003776c86c22ee903bfb6cff
0xBigfish <46397578+0xBigfish@users.noreply.github.com>
2019-10-22 22:14:55 +0200

    Player is now a physical object
    Player can't move through walls anymore
    
    Second upload of this commit as the merge of branch "development" and "development2.0" rendered the branch broken

--- mini_project/Assets/Scripts/Player/PayerMouvements.cs
+++ mini_project/Assets/Scripts/Player/PayerMouvements.cs
@@ -6,43 +6,33 @@ public class PayerMouvements : MonoBehaviour
 {
     private float horizontalInput;
     private float verticalInput;
-    private float speed = 5.0f;
+    public float speed = 3.0f;
     public float xBoundary = 15.0f;
     public float zBoundary = 7.2f; //standard actual values
     private float zIntervalForOutOfBound;
     private float xIntervalForOutOfBound = 20.0f;
     //private Vector3 thePointWherePlayerSchouldBeTeleported;
 
+    public Rigidbody body;
     public GameObject boullet;
 
     // Start is called before the first frame update
     void Start()
     {
         //thePointWherePlayerSchouldBeTeleported = new Vector3(xIntervalForOutOfBound, 0.0f, zIntervalForOutOfBound);
+        body = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x > xBoundary) 
-        {
-            transform.position = new Vector3(xBoundary, transform.position.y, transform.position.z);
-        }
-        if (transform.position.x < -xBoundary)
-        {
-            transform.position = new Vector3(-xBoundary, transform.position.y, transform.position.z);
-        }
-        if (transform.position.z < -3 * zBoundary)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, -3 * zBoundary);
-        }
-        if (transform.position.z > zBoundary + 10.0f)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, zBoundary + 10.0f);
-        }    
+        // Reads user input from WASD and the arrow key
+        verticalInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
-        //rotate player
-        transform.Rotate(0.0f, horizontalInput * 3.0f, 0.0f);
+
+        // Applies input onto the velocity of the player's rigidbody to move the player
+        body.velocity = transform.forward * speed * verticalInput * Time.deltaTime;
+        transform.Rotate(0, horizontalInput * 3, 0);
 
         verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
