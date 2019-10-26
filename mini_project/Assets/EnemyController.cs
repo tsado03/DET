6a9e68face04469a85510f8e9f79d6ac1e06b72c
0xBigfish <0xBigfish@users.noreply.github.com>
2019-10-26 20:11:24 +0200

    Added NavMesh and first enemy movement
    Added NavMesh
     - Changed "Plane" dimensions to fit better to the wall desion
    
    Added EnemyController Skript
     - Bug: Can't find player, just walks to point (0,0,0)

---
+++ mini_project/Assets/EnemyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    //NavMeshAgent steuern den Enemy über das NavMesh
+    public NavMeshAgent agent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Der Enemy soll immer zum Player laufen. BUG: LIEFERT AKTUELL NUR NULL
+        //Vector3 test = GameObject.FindWithTag("Player").transform.position;
+
+        //Läuft aktuell nur zum Punkt (0,0,0) da der Player nicht gefunden wird
+        agent.SetDestination(Vector3.zero);
+    }
+}
