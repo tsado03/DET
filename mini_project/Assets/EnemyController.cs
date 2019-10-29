1162d29e3b95aa9edb37f10d960da7cae88173a9
DET Labor ulexit <ulexit@det>
2019-10-29 14:16:42 +0100

    Corrected Enemy Movement
    Enemy now follows a set target

--- mini_project/Assets/EnemyController.cs
+++ mini_project/Assets/EnemyController.cs
@@ -7,6 +7,7 @@ public class EnemyController : MonoBehaviour
 {
     //NavMeshAgent steuern den Enemy über das NavMesh
     public NavMeshAgent agent;
+    public GameObject target;
 
 
     // Start is called before the first frame update
@@ -18,10 +19,8 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Der Enemy soll immer zum Player laufen. BUG: LIEFERT AKTUELL NUR NULL
-        //Vector3 test = GameObject.FindWithTag("Player").transform.position;
-
-        //Läuft aktuell nur zum Punkt (0,0,0) da der Player nicht gefunden wird
-        agent.SetDestination(Vector3.zero);
+        // Sucht das target GameObject und bewegt sich zu ihm
+        Vector3 vecotToTarget = target.transform.position;
+        agent.SetDestination(vecotToTarget);
     }
 }
